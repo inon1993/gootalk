16a797894fd3202f020be8e8037ae1e5cd653762
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-05-04 12:43:40 +0300

    add home and navbar, change font to roboto. icons not showing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import Home from "./pages/Home/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { AccountCircle } from "@mui/icons-material"
+import { Person } from "@mui/icons-material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Navbar = () => {
+    return (
+        <Person />
+    )
+}
+
+export default Navbar;
