68b05e72b6e8e6b5e672332f484368e0fb6f416b
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-05-23 14:30:13 +0300

    add mobile menu.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import classes from "./Navbar.module.css";
-import { HomeRounded, MessageRounded } from "@mui/icons-material";
+import { Close, HomeRounded, MenuRounded, MessageRounded } from "@mui/icons-material";
 import Search from "./Search/Search";
 import Profile from "./Profile/Profile";
 import DropdownMenu from "./Profile/DropdownMenu/DropdownMenu";
@@ -14,71 +14,86 @@ const Navbar = () => {
   const isActivated = useSelector((state) => state.navbar.activate);
   const isActivatedDropdown = useSelector((state) => state.dropdown.activate);
   const dispatch = useDispatch();
+  const [isMobileMenu, setIsMobileMenu] = useState(false);
 
   const activateHomeHandler = () => {
     dispatch(navbarActions.activateHome());
     dispatch(menuActions.activateHome());
     dispatch(dropdownActions.deactivate());
-    // setIsDropdown(false);
   };
 
   const activateNotificationHandler = () => {
     dispatch(navbarActions.activateNotification());
     dispatch(dropdownActions.deactivate());
     dispatch(menuActions.deactivate());
-    // setIsDropdown(false);
   };
 
   const activateProfileHandler = () => {
     dispatch(dropdownActions.activate());
     dispatch(navbarActions.activateProfile());
-    // setIsDropdown(!isDropdown);
-    // dispatch(menuActions.deactivate())
   };
 
   const disabaleDropdownHandler = () => {
-    // setIsDropdown(false);
     dispatch(dropdownActions.deactivate());
     dispatch(navbarActions.activateProfile());
   };
 
+  const mobileMenuHandler = () => {
+    setIsMobileMenu(!isMobileMenu);
+  };
+
   return (
-    <div className={classes.navbar}>
-      <div className={classes.logo}>
-        <h1 className={classes["navbar-logo"]}>
-          Goo<span className={classes["logo-span"]}>talk</span>
-        </h1>
+    <>
+      <div className={classes.navbar}>
+        <div className={classes.logo}>
+          <h1 className={classes["navbar-logo"]}>
+            Goo<span className={classes["logo-span"]}>talk</span>
+          </h1>
+        </div>
+        <div className={classes.search}>
+          <Search />
+        </div>
+        <div className={classes["navbar-features"]}>
+          <HomeRounded
+            onClick={activateHomeHandler}
+            className={
+              isActivated.home
+                ? classes["navbar-features-icon-focus"]
+                : classes["navbar-features-icon"]
+            }
+          />
+          <MessageRounded
+            onClick={activateNotificationHandler}
+            className={
+              isActivated.notifications
+                ? classes["navbar-features-icon-focus"]
+                : classes["navbar-features-icon"]
+            }
+          />
+          <Profile onClick={activateProfileHandler} activate={isActivated} />
+          {isActivatedDropdown && (
+            <div className={classes["dropdown-menu"]}>
+              <DropdownBackground onClose={disabaleDropdownHandler}>
+                <DropdownMenu />
+              </DropdownBackground>
+            </div>
+          )}
+        </div>
+        <div className={classes["mobile-menu"]} onClick={mobileMenuHandler}>
+          {!isMobileMenu && <MenuRounded className={classes["mm-icon"]} />}
+          {isMobileMenu && <Close className={classes["mm-icon"]} />}
+        </div>
       </div>
-      <div className={classes.search}>
-        <Search />
-      </div>
-      <div className={classes["navbar-features"]}>
-        <HomeRounded
-          onClick={activateHomeHandler}
-          className={
-            isActivated.home
-              ? classes["navbar-features-icon-focus"]
-              : classes["navbar-features-icon"]
-          }
-        />
-        <MessageRounded
-          onClick={activateNotificationHandler}
-          className={
-            isActivated.notifications
-              ? classes["navbar-features-icon-focus"]
-              : classes["navbar-features-icon"]
-          }
-        />
-        <Profile onClick={activateProfileHandler} activate={isActivated} />
-        {isActivatedDropdown && (
-          <div className={classes["dropdown-menu"]}>
-            <DropdownBackground onClose={disabaleDropdownHandler}>
-              <DropdownMenu />
-            </DropdownBackground>
-          </div>
-        )}
-      </div>
-    </div>
+      {isMobileMenu && <DropdownBackground onClose={mobileMenuHandler}>
+        <div className={classes["mm-features"]}>
+          <ul className={classes["mm-ul"]}>
+            <li className={classes["mm-search"]}>
+              <Search />
+            </li>
+          </ul>
+        </div>
+      </DropdownBackground>}
+    </>
   );
 };
 
