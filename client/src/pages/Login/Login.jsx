b0e21f7924bd098922ef336252546386e01b1e4d
inon1993 <inon1993@gmail.com>
2022-05-26 01:21:21 +0300

    finish basic design for login page.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,29 +1,56 @@
-import Card from "../../components/UI/Card/Card";
 import classes from "./Login.module.css";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
 
 const Login = () => {
+  const [isVisiblePw, setISVisiblePw] = useState(false);
+  const [isPwInput, setIsPwInput] = useState("password");
+
+  const visibilityHandler = () => {
+    setISVisiblePw(true);
+    setIsPwInput("text");
+  };
+
+  const visibilityOffHandler = () => {
+    setISVisiblePw(false);
+    setIsPwInput("password");
+  };
   return (
-    <div className={classes.login}>
-        <div className={classes["login-welcome"]}>
-          <span className={classes["login-welcome-text"]}>
-            Welcome to Gootalk!
-          </span>
-        </div>
-      <div className={classes["login-box"]}>
-        
-        <div className={classes["login-form-wrapper"]}>
-          <form className={classes["login-form"]}>
-            <h2 className={classes["login-title"]}>Login</h2>
-            <span className={classes["login-form-text"]}>E-Mail</span>
-            <input className={classes["login-input"]} type="email" />
-            <span className={classes["login-form-text"]}>Password</span>
-            <input className={classes["login-input"]} type="password" />
-            <button className={classes["login-button"]}>Login</button>
-          </form>
-          <div className={classes["login-signup"]}>
-            <span>Not a user yet?</span>
-            <button className={classes["login-signup-button"]}>Sign Up</button>
+    <div className={classes["login-wrapper"]}>
+      <div className={classes["login-left"]}>
+        <h2 className={classes["ll-text"]}>Let's have a</h2>
+        <h1 className={classes["ll-goo"]}>
+          Goo<span className={classes["ll-talk"]}>talk</span>
+        </h1>
+      </div>
+      <div className={classes["login-right"]}>
+        <h2 className={classes["lr-title"]}>Login</h2>
+        <form className={classes["lr-form"]}>
+          <span className={classes["lr-form-text"]}>E-Mail</span>
+          <input className={classes["lr-input"]} type="text" />
+          <span className={classes["lr-form-text"]}>Password</span>
+          <div className={classes["lr-input"]}>
+            <input className={classes["lr-input-password"]} type={isPwInput} />
+            {!isVisiblePw && (
+              <Visibility
+                className={classes["lr-visibility"]}
+                onClick={visibilityHandler}
+              />
+            )}
+            {isVisiblePw && (
+              <VisibilityOff
+                className={classes["lr-visibility"]}
+                onClick={visibilityOffHandler}
+              />
+            )}
           </div>
+          <button className={classes["lr-login-button"]}>Login</button>
+        </form>
+        <div className={classes["lr-signup"]}>
+          <span className={classes["lr-s-text"]}>
+            Don't have an account yet?
+          </span>
+          <button className={classes["lr-signup-button"]}>Sign Up</button>
         </div>
       </div>
     </div>
