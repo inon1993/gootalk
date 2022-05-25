3170f7bfd5a5bda0fd89344d7e2bebf6adb65520
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-05-25 17:06:54 +0300

    trying out a login design.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -4,23 +4,26 @@ import classes from "./Login.module.css";
 const Login = () => {
   return (
     <div className={classes.login}>
-      <div className={classes["login-box"]}>
         <div className={classes["login-welcome"]}>
-          <span>Welcome to Gootalk!</span>
+          <span className={classes["login-welcome-text"]}>
+            Welcome to Gootalk!
+          </span>
         </div>
+      <div className={classes["login-box"]}>
+        
         <div className={classes["login-form-wrapper"]}>
-          <h2 className={classes["login-title"]}>Login</h2>
           <form className={classes["login-form"]}>
+            <h2 className={classes["login-title"]}>Login</h2>
             <span className={classes["login-form-text"]}>E-Mail</span>
-            <input className={classes["login-input"]} type="text" />
+            <input className={classes["login-input"]} type="email" />
             <span className={classes["login-form-text"]}>Password</span>
             <input className={classes["login-input"]} type="password" />
-            <button className={classes["login-button"]} type="button">
-              Login
-            </button>
+            <button className={classes["login-button"]}>Login</button>
+          </form>
+          <div className={classes["login-signup"]}>
             <span>Not a user yet?</span>
             <button className={classes["login-signup-button"]}>Sign Up</button>
-          </form>
+          </div>
         </div>
       </div>
     </div>
