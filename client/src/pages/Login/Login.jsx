a96078611f9c99a57bcc18a0bc7ee15c7c1a7747
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-05-26 13:03:54 +0300

    finish login page design. move login form to a new component.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,20 +1,7 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
 import classes from "./Login.module.css";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import { useState } from "react";
 
 const Login = () => {
-  const [isVisiblePw, setISVisiblePw] = useState(false);
-  const [isPwInput, setIsPwInput] = useState("password");
-
-  const visibilityHandler = () => {
-    setISVisiblePw(true);
-    setIsPwInput("text");
-  };
-
-  const visibilityOffHandler = () => {
-    setISVisiblePw(false);
-    setIsPwInput("password");
-  };
   return (
     <div className={classes["login-wrapper"]}>
       <div className={classes["login-left"]}>
@@ -24,34 +11,7 @@ const Login = () => {
         </h1>
       </div>
       <div className={classes["login-right"]}>
-        <h2 className={classes["lr-title"]}>Login</h2>
-        <form className={classes["lr-form"]}>
-          <span className={classes["lr-form-text"]}>E-Mail</span>
-          <input className={classes["lr-input"]} type="text" />
-          <span className={classes["lr-form-text"]}>Password</span>
-          <div className={classes["lr-input"]}>
-            <input className={classes["lr-input-password"]} type={isPwInput} />
-            {!isVisiblePw && (
-              <Visibility
-                className={classes["lr-visibility"]}
-                onClick={visibilityHandler}
-              />
-            )}
-            {isVisiblePw && (
-              <VisibilityOff
-                className={classes["lr-visibility"]}
-                onClick={visibilityOffHandler}
-              />
-            )}
-          </div>
-          <button className={classes["lr-login-button"]}>Login</button>
-        </form>
-        <div className={classes["lr-signup"]}>
-          <span className={classes["lr-s-text"]}>
-            Don't have an account yet?
-          </span>
-          <button className={classes["lr-signup-button"]}>Sign Up</button>
-        </div>
+          <LoginForm />
       </div>
     </div>
   );
