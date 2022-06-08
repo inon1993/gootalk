70bb9f0daf9ddd4e748b1f2ab444fff256ee266c
inon1993 <inon1993@gmail.com>
2022-06-08 23:22:46 +0300

    start redirecting if user is set in store - not working yet.

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,26 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Redirect,
 } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import ProfilePage from "./pages/Profile/ProfilePage";
 import Login from "./pages/Login/Login";
 import "./App.css";
 import Signup from "./pages/Signup/Signup";
+import { useSelector } from "react-redux";
 
 function App() {
+  const user = useSelector((state) => state.user.email);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        {user !== "" ? (
+          <Route path="/" element={<HomePage />} />
+        ) : (
+          <Redirect to="/login" />
+        )}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile/:username" element={<ProfilePage />} />
