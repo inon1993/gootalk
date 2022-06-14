5518af1e6b4ae64204f11b58aa0a62e774c7949a
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-06-14 13:13:43 +0300

    move jwt secret to env.

--- middleware/auth-mid.js
+++ middleware/auth-mid.js
@@ -5,7 +5,7 @@ const User = require("../models/User")
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'thisismynewapp');
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const user = await User.findOne({_id: decoded._id, 'tokens.token': token});
 
     if(!user) {
--- models/User.js
+++ models/User.js
@@ -64,7 +64,7 @@ const UserSchema = new mongoose.Schema(
 
 UserSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewapp');
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.ACCESS_TOKEN_SECRET);
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
