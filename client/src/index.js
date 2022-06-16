882fb3e875b5054f2cf3c7da633b0404e1e1032a
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-06-16 15:57:16 +0300

    implement redux persist .

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from "./store/index";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/index";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider store={store}>
-    <App />
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,16 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import menuReducer from "./menu-slice";
 import navbarReducer from "./navbar-slice";
 import dropdownReducer from "./dropdown-slice";
 import userReducer from "./user-slice";
 
-const store = configureStore({
-  reducer: {
-    menu: menuReducer,
-    navbar: navbarReducer,
-    dropdown: dropdownReducer,
-    user: userReducer,
-  },
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  menu: menuReducer,
+  navbar: navbarReducer,
+  dropdown: dropdownReducer,
+  user: userReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk]
+});
+
+export const persistor = persistStore(store)
