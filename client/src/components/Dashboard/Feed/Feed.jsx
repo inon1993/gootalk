c68d1d02f9695b8ce9acf371dbe9743c355f4179
inon1993 <inon1993@gmail.com>
2022-06-20 01:13:49 +0300

    create useRefreshToken hook to refresh access token.

--- client/src/components/Dashboard/Feed/Feed.jsx
+++ client/src/components/Dashboard/Feed/Feed.jsx
@@ -4,20 +4,24 @@ import Post from "./Post/Post";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import useRefreshToken from "../../../hooks/useRefreshToken";
+import { Refresh } from "@mui/icons-material";
 
 const Feed = () => {
   const user = useSelector((state) => state.user);
   const [posts, setPosts] = useState([]);
 
+  const refresh = useRefreshToken();
+
   useEffect(() => {
     const getPosts = async () => {
       console.log(user);
-      const postsArray = await axios.get(
-        `/post/timeline/${user.userId}`, {
-          headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmE4NjlkYzFkMjhiNmRmMWQ2YTNlY2EiLCJpYXQiOjE2NTUyMDQzMTZ9.F3IL4syKcEDTlh-yNCGf3fnWDgd2jIcOCVgBtMCl7C0'
-          }
-        });
+      const postsArray = await axios.get(`/post/timeline/${user.userId}`, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmE4NjlkYzFkMjhiNmRmMWQ2YTNlY2EiLCJpYXQiOjE2NTUyMDQzMTZ9.F3IL4syKcEDTlh-yNCGf3fnWDgd2jIcOCVgBtMCl7C0",
+        },
+      });
       setPosts(postsArray.data);
     };
 
@@ -27,6 +31,13 @@ const Feed = () => {
   return (
     <div className={classes.feed}>
       <NewPost />
+      <button
+        onClick={() => {
+          refresh();
+        }}
+      >
+        Refresh
+      </button>
       {posts.map((post) => {
         return <Post post={post} />;
       })}
---
+++ client/src/hooks/useRefreshToken.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "../store/user-slice";
+
+const useRefreshToken = () => {
+  const user = useSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+  const refresh = async () => {
+    const response = await axios.get("/refresh", {
+      withCredentials: true,
+    });
+    dispatch(
+      userActions.setUser({
+        userId: user.userId,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+        profilePicture: user.profilePicture,
+        coverPicture: user.coverPicture,
+        followers: user.followers,
+        following: user.following,
+        country: user.country,
+        city: user.city,
+        accessToken: response.data.accessToken,
+      })
+    );
+    return response.data.accessToken;
+  };
+  return refresh;
+};
+
+export default useRefreshToken;
