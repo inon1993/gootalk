f81164294d50118afe6445ecbb739b8b93a7fbd0
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-07-25 12:00:44 +0300

    add cors middleware.

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ mongoose.connection.on("connected", () => {
 app.use(cors({
   // origin: "http://gootalk.herokuapp.com", 
   origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
 }));
 
