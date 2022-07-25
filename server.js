dfa5b2d5dd8b1686196f30ad3b48bb9e0d35f634
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-07-25 10:01:47 +0300

    add cors middleware.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const cors = require("cors")
 
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
@@ -22,6 +23,8 @@ mongoose.connect(
 mongoose.connection.on("connected", () => {
   console.log("Mongoose is connected.");
 });
+
+app.use(cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
