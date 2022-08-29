cb9a7c77d4ef03a51a7d293da9adbd9835d36c5d
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-08-29 14:16:52 +0300

    add update function, not finished.

--- client/src/components/EditProfile/EditProfile.jsx
+++ client/src/components/EditProfile/EditProfile.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useState, useEffect, isValidElement } from "react";
 import classes from "./EditProfile.module.css";
 import ppIcon from "../../images/pp-icon-biggest.png";
 import coverImg from "../../images/gootalk-cover.jpg";
@@ -8,8 +8,11 @@ import {
   CitySelector,
 } from "../SignupForm/LocationSelector/LocationSelector";
 import getLocations from "../../helpers/countries-api/getLocations";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { useDispatch } from "react-redux";
+import { userActions } from "../../store/user-slice";
 
-const EditProfile = ({onCloseEdit}) => {
+const EditProfile = ({ onCloseEdit }) => {
   const user = useSelector((state) => state.user.user);
   const [countries, setCountries] = useState([]);
   const [countryFocus, setCountryFocus] = useState(false);
@@ -27,6 +30,12 @@ const EditProfile = ({onCloseEdit}) => {
     country: user.country,
     city: user.city,
   });
+
+  const [isValid, setIsValid] = useState({firstname: null, lastname: true});
+  const [isLoading, setIsLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState({ code: null, msg: "" });
+  const req = useAxiosPrivate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getCountriesList = async () => {
@@ -49,8 +58,8 @@ const EditProfile = ({onCloseEdit}) => {
         ...prevState,
         firstname: e.target.value,
       };
-    })
-  }
+    });
+  };
 
   const updateLasttnameHandler = (e) => {
     setUpdatedUser((prevState) => {
@@ -58,8 +67,8 @@ const EditProfile = ({onCloseEdit}) => {
         ...prevState,
         lastname: e.target.value,
       };
-    })
-  }
+    });
+  };
 
   const countryHandler = (e) => {
     setCountry(e.target.value);
@@ -82,113 +91,144 @@ const EditProfile = ({onCloseEdit}) => {
     setCountryObj(null);
   };
 
+  const updateHandler = async (e) => {
+    e.preventDefault();
+    // if(!isValid.firstname || !isValid.lastname) {
+    //   return;
+    // }
+    setIsLoading(true);
+    setErrMsg({ code: null, msg: "" });
+    try {
+      await req.put(`/user/${user.userId}`, {...updatedUser, userId: user.userId})
+      dispatch(userActions.setUser({...updatedUser, userId: user.userId}));
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      if(error.response.status === 500) {
+        setErrMsg({
+          code: 500,
+          msg: "Something went wrong. Please try again.",
+        })
+      }
+    }
+  };
+
   const cancelHandler = () => {
-    onCloseEdit(false)
-  }
+    onCloseEdit(false);
+  };
 
   return (
     <div className={classes["edit-wrapper"]}>
-      <div className={classes["profile-cover-wrapper"]}>
-        <span className={classes["edit-text"]}>Cover picture:</span>
-        <img
-          className={classes["profile-cover"]}
-          src={user.coverPicture || coverImg}
-          alt="cover"
-        />
-      </div>
-      <div className={classes["profile-picture"]}>
-        <span className={classes["edit-text"]}>Profile picture:</span>
-        <img
-          className={classes["profile-pic"]}
-          src={user.profilePicture || ppIcon}
-          alt="cover"
-        />
-      </div>
-      <div className={classes["edit-firstname-lastname"]}>
-        <span className={classes["edit-text"]}>First name:</span>
-        <input
-          className={classes["edit-input"]}
-          type="text"
-          defaultValue={user.firstname}
-          onChange={updateFirstnameHandler}
-        />
-      </div>
-      <div className={classes["edit-firstname-lastname"]}>
-        <span className={classes["edit-text"]}>Last name:</span>
-        <input
-          className={classes["edit-input"]}
-          type="text"
-          defaultValue={user.lastname}
-          onChange={updateLasttnameHandler}
-        />
-      </div>
-      <div className={classes["edit-country-city-wrapper"]}>
-        <span className={classes["edit-text"]}>Country:</span>
-        <div className={classes["edit-country-city"]}>
-          <input
-            className={`${classes["edit-input-country-city"]}`}
-            name="country"
-            type="text"
-            value={country}
-            onChange={countryHandler}
-            onFocus={() => {
-              setCountryFocus(true);
-            }}
-            onBlur={() => setCountryFocus(false)}
-            autoComplete="none"
-            defaultValue={user.country}
+      <form className={classes["edit-form"]} onSubmit={updateHandler}>
+        <div className={classes["profile-cover-wrapper"]}>
+          <span className={classes["edit-text"]}>Cover picture:</span>
+          <img
+            className={classes["profile-cover"]}
+            src={user.coverPicture || coverImg}
+            alt="cover"
           />
-          {countryFocus && (
-            <CountrySelector
-              country={country}
-              countries={countries}
-              onSetCountryObj={setCountryObj}
-              onSetCountry={setCountry}
-              onSetUser={setUpdatedUser}
-              // onEmptyCountries={setIsEmptyCountries}
-            />
-          )}
         </div>
-      </div>
-      <div className={classes["edit-country-city-wrapper"]}>
-        <span className={classes["edit-text"]}>City</span>
-        <div className={classes["edit-country-city"]}>
-          <input
-            className={`${classes["edit-input-country-city"]} ${
-              country === "" && classes["disabled"]
-            }`}
-            name="city"
-            type="text"
-            value={city}
-            onChange={(e) => {
-              setCity(e.target.value);
-              setUpdatedUser((prevState) => {
-                return {
-                  ...prevState,
-                  city: e.target.value,
-                };
-              });
-            }}
-            onFocus={() => setCityFocus(true)}
-            onBlur={() => setCityFocus(false)}
-            autoComplete="none"
-            defaultValue={user.city}
-            disabled={country === "" ? true : false}
+        <div className={classes["profile-picture"]}>
+          <span className={classes["edit-text"]}>Profile picture:</span>
+          <img
+            className={classes["profile-pic"]}
+            src={user.profilePicture || ppIcon}
+            alt="cover"
           />
-          {cityFocus && countryObj && (
-            /*city !== "" &&*/ <CitySelector
-              city={city}
-              country={countryObj}
-              onSetCity={setCity}
-              onSetUser={setUpdatedUser}
-            />
-          )}
         </div>
-      </div>
-      <div className={classes["edit-buttons-wrapper"]}>
-        <button className={classes["edit-save-btn"]} onClick={() => console.log(updatedUser)}>Save</button>
-        <button className={classes["edit-cancel-btn"]} onClick={cancelHandler}>Cancel</button>
-      </div>
+        <div className={classes["edit-firstname-lastname"]}>
+          <span className={classes["edit-text"]}>First name:</span>
+          <input
+            className={classes["edit-input"]}
+            type="text"
+            defaultValue={user.firstname}
+            onChange={updateFirstnameHandler}
+          />
+        </div>
+        <div className={classes["edit-firstname-lastname"]}>
+          <span className={classes["edit-text"]}>Last name:</span>
+          <input
+            className={classes["edit-input"]}
+            type="text"
+            defaultValue={user.lastname}
+            onChange={updateLasttnameHandler}
+          />
+        </div>
+        <div className={classes["edit-country-city-wrapper"]}>
+          <span className={classes["edit-text"]}>Country:</span>
+          <div className={classes["edit-country-city"]}>
+            <input
+              className={`${classes["edit-input-country-city"]}`}
+              name="country"
+              type="text"
+              value={country}
+              onChange={countryHandler}
+              onFocus={() => {
+                setCountryFocus(true);
+              }}
+              onBlur={() => setCountryFocus(false)}
+              autoComplete="none"
+              defaultValue={user.country}
+            />
+            {countryFocus && (
+              <CountrySelector
+                country={country}
+                countries={countries}
+                onSetCountryObj={setCountryObj}
+                onSetCountry={setCountry}
+                onSetUser={setUpdatedUser}
+                // onEmptyCountries={setIsEmptyCountries}
+              />
+            )}
+          </div>
+        </div>
+        <div className={classes["edit-country-city-wrapper"]}>
+          <span className={classes["edit-text"]}>City</span>
+          <div className={classes["edit-country-city"]}>
+            <input
+              className={`${classes["edit-input-country-city"]} ${
+                country === "" && classes["disabled"]
+              }`}
+              name="city"
+              type="text"
+              value={city}
+              onChange={(e) => {
+                setCity(e.target.value);
+                setUpdatedUser((prevState) => {
+                  return {
+                    ...prevState,
+                    city: e.target.value,
+                  };
+                });
+              }}
+              onFocus={() => setCityFocus(true)}
+              onBlur={() => setCityFocus(false)}
+              autoComplete="none"
+              defaultValue={user.city}
+              disabled={country === "" ? true : false}
+            />
+            {cityFocus && countryObj && (
+              /*city !== "" &&*/ <CitySelector
+                city={city}
+                country={countryObj}
+                onSetCity={setCity}
+                onSetUser={setUpdatedUser}
+              />
+            )}
+          </div>
+        </div>
+        <div className={classes["edit-buttons-wrapper"]}>
+          <button className={classes["edit-save-btn"]}>
+            Save
+          </button>
+          <button
+            className={classes["edit-cancel-btn"]}
+            onClick={cancelHandler}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
