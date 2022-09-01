5785991ef8f0df4277d0541146eec46382aa96fd
Shmuel Moallem <ShmuelM@Hy-Labs.local>
2022-09-01 14:21:25 +0300

    resize friends and users dashboard. add settings layout.

--- client/src/components/Dashboard/DashboardSettings/DashboardSettings.jsx
+++ client/src/components/Dashboard/DashboardSettings/DashboardSettings.jsx
@@ -1,8 +1,15 @@
 import classes from "./DashboardSettings.module.css"
+import Card from "../../UI/Card/Card"
 
 const DashboardSettings = () => {
     return (
-        <div></div>
+        <Card className={classes["settings-wrapper"]}>
+            <div className={classes["user-settings"]}>
+                <span className={classes["settings-title"]}>User Settings</span>
+                <button className={classes["settings-btn"]}>Change e-mail</button>
+                <button className={classes["settings-btn"]}>Change password</button>
+            </div>
+        </Card>
     )
 }
 
